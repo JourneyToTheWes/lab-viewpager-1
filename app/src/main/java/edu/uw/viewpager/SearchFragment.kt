9d9baaf6b485f5d6bd7e7953cc626fd00ebf28dc
journeytothewes <wesley631w@gmail.com>
2018-10-24 13:25:47 -0700

    Added the SearchFragment.

---
+++ app/src/main/java/edu/uw/viewpager/SearchFragment.kt
@@ -0,0 +1,70 @@
+package edu.uw.viewpager
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+
+import edu.uw.fragmentdemo.R
+import kotlinx.android.synthetic.main.notification_template_lines_media.view.*
+
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
+
+/**
+ * A simple [Fragment] subclass.
+ * Activities that contain this fragment must implement the
+ * [SearchFragment.OnFragmentInteractionListener] interface
+ * to handle interaction events.
+ * Use the [SearchFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ *
+ */
+class SearchFragment : Fragment() {
+    private var callback: OnSearchListener? = null
+
+    internal interface OnSearchListener {
+        fun onSearchSubmitted(searchTerm: String)
+    }
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+        // Inflate the layout for this fragment
+        val rootView = inflater.inflate(R.layout.fragment_search, container, false)
+
+        val searchbtn = rootView.findViewById<Button>(R.id.btn_search)
+        searchbtn.setOnClickListener {
+            val text = rootView.findViewById<View>(R.id.txt_search)
+            val searchTxt = text.text.toString()
+            callback!!.onSearchSubmitted(searchTxt)
+        }
+
+        return rootView
+    }
+
+    override fun onAttach(context: Context) {
+        super.onAttach(context)
+
+        try {
+            callback = context as OnSearchListener?
+        } catch (e: ClassCastException) {
+            throw ClassCastException(context!!.toString() + " does not implement OnSearchListener")
+        }
+    }
+
+    companion object {
+
+        fun newInstance(): SearchFragment {
+            val args = Bundle()
+            val fragment = SearchFragment()
+            fragment.arguments = args
+            return fragment
+        }
+    }
+}
